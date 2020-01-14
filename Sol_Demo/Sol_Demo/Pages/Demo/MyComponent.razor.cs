f1401b5a9792c35964a2a4b41224f5ed41d32fcd
KishorNaik <kishor.naik011.net@gmail.com>
2020-01-14 08:46:43 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Pages/Demo/MyComponent.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Pages.Demo
+{
+    public partial class MyComponent
+    {
+        private int counter = 0;
+
+        public void Increment()
+        {
+            counter++;
+        }
+    }
+}
