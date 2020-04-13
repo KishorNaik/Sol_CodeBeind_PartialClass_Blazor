17f83e4634c2a397b9efd45d45b28c714decc97a
KishorNaik <kishor.naik011.net@gmail.com>
2020-04-13 12:05:17 +0530

    Code Commit

--- Sol_Demo/Sol_Demo/Pages/Demo/MyComponent.razor.cs
+++ Sol_Demo/Sol_Demo/Pages/Demo/MyComponent.razor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,9 @@ namespace Sol_Demo.Pages.Demo
     public partial class MyComponent
     {
         private int counter = 0;
+
+        [Parameter]
+        public String Title { get; set; }
 
         public void Increment()
         {
